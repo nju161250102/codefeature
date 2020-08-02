0f8031801cc1998ec261553e7ff27a61f16cae97
nju161250102 <161250102@smail.nju.edu.cn>
2020-08-02 11:27:55 +0800

    Update: file not exist

--- server/python/SVM.py
+++ server/python/SVM.py
@@ -20,10 +20,23 @@ def evaluate(y_data, y_predict, txt_info):
     print("f1: " + str(f1_score(y_data, y_predict)))
 
 
+def data_split(data_dir, feature_type="WordVector"):
+    file_data = []
+    y_data = []
+    for file_name, label in get_feature_files(data_dir, feature_type):
+        file_data.append(file_name)
+        y_data.append(1 if label == "False" else 0)
+
+    print(feature_type + ": " + str(len(file_data)))
+    return train_test_split(file_data, y_data, test_size=0.3)
+
+
 def vector_seq(file_list, y_list, data_dir, feature_type="WordVector", compress=False):
     x_data = []
     for i in range(0, len(file_list)):
         file_path = os.path.join(data_dir, "False" if y_list[i] == 1 else "Positive", feature_type, file_list[i])
+        if os.path.getsize(file_path) == 0:
+            continue
         x_batch = read_feature_file(file_path, feature_type, get_config("seq_len"))
         if compress:
             x_data.append([sum(batch) / len(batch) for batch in x_batch])
@@ -37,6 +50,8 @@ def vector_graph(file_list, y_list, data_dir, feature_type):
     x_data = []
     for i in range(0, len(file_list)):
         file_path = os.path.join(data_dir, "False" if y_list[i] == 1 else "Positive", feature_type, file_list[i])
+        if os.path.getsize(file_path) == 0:
+            continue
         edge_batch = read_feature_file(file_path, "Edge", get_config("node_len"))
         x_batch = read_feature_file(file_path, feature_type, get_config("node_len"))
         edge_data.append(edge_batch)
@@ -79,42 +94,28 @@ def nn(model_name, model, X_train, X_test, y_train, y_test):
 
 
 if __name__ == "__main__":
-    file_data = []
-    y_data = []
-    # data_path =
-    for file_name, label in get_feature_files(sys.argv[1]):
-        file_data.append(file_name)
-        y_data.append(1 if label == "False" else 0)
-    file_train, file_test, y_train, y_test = train_test_split(file_data, y_data, test_size=0.3)
-
-    print(len(file_data))
+    file_train, file_test, y_train, y_test = data_split(sys.argv[1])
     # svm and nb
-    X_train = vector_seq(file_train, y_train, sys.argv[1], compress=True)
-    X_test = vector_seq(file_test, y_test, sys.argv[1], compress=True)
-    svm(X_train, X_test, y_train, y_test)
-    gaussianNB(X_train, X_test, y_train, y_test)
+    #X_train = vector_seq(file_train, y_train, sys.argv[1], compress=True)
+    #X_test = vector_seq(file_test, y_test, sys.argv[1], compress=True)
+    #svm(X_train, X_test, y_train, y_test)
+    #gaussianNB(X_train, X_test, y_train, y_test)
 
     # nn
-    for model_name in ["cnn", "lstm", "deepwalk", "para2vec"]:
-        if model_name == "cnn" or model_name == "lstm":
-            X_train = vector_seq(file_train, y_train, sys.argv[1])
-            X_test = vector_seq(file_test, y_test, sys.argv[1])
-            model = cnn_model(16)
-        elif model_name == "lstm":
-            X_train = vector_seq(file_train, y_train, sys.argv[1])
-            X_test = vector_seq(file_test, y_test, sys.argv[1])
-            model = lstm_model(16)
-        elif model_name == "deepwalk":
-            X_train = vector_graph(file_train, y_train, sys.argv[1], "DeepWalk")
-            X_test = vector_graph(file_test, y_test, sys.argv[1], "DeepWalk")
-            model = gcn_model(16)
-        elif model_name == "para2vec":
-            X_train = vector_graph(file_train, y_train, sys.argv[1], "ParagraphVec")
-            X_test = vector_graph(file_test, y_test, sys.argv[1], "ParagraphVec")
-            model = gcn_model(16)
-        else:
-            continue
-
-        nn(model_name, model, X_train, X_test, y_train, y_test)
+    #X_train = vector_seq(file_train, y_train, sys.argv[1])
+    #X_test = vector_seq(file_test, y_test, sys.argv[1])
+    #nn("cnn", cnn_model(16), X_train, X_test, y_train, y_test)
+    #
+    #nn("lstm", lstm_model(16), X_train, X_test, y_train, y_test)
+    #
+    file_train, file_test, y_train, y_test = data_split(sys.argv[1], "DeepWalk")
+    X_train = vector_graph(file_train, y_train, sys.argv[1], "DeepWalk")
+    X_test = vector_graph(file_test, y_test, sys.argv[1], "DeepWalk")
+    nn("deepwalk_gcn", gcn_model(16), X_train, X_test, y_train, y_test)
+    #
+    file_train, file_test, y_train, y_test = data_split(sys.argv[1], "ParagraphVec")
+    X_train = vector_graph(file_train, y_train, sys.argv[1], "ParagraphVec")
+    X_test = vector_graph(file_test, y_test, sys.argv[1], "ParagraphVec")
+    nn("dpara2vec_gcn", gcn_model(16), X_train, X_test, y_train, y_test)
 
 
