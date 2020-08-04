35a8cb73a3ab6482ad900981c6d4efba891f05af
nju161250102 <161250102@smail.nju.edu.cn>
2020-08-04 13:12:24 +0800

    Add: feature fusion

--- server/python/SVM.py
+++ server/python/SVM.py
@@ -1,6 +1,8 @@
 import numpy as np
+from sklearn import preprocessing
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
 from keras.engine.saving import load_model
@@ -35,14 +37,32 @@ def vector_seq(file_list, y_list, data_dir, feature_type="WordVector", compress=
     x_data = []
     for i in range(0, len(file_list)):
         file_path = os.path.join(data_dir, "False" if y_list[i] == 1 else "Positive", feature_type, file_list[i])
-        if os.path.getsize(file_path) == 0:
-            continue
         x_batch = read_feature_file(file_path, feature_type, get_config("seq_len"))
         if compress:
             x_data.append([sum(batch) / len(batch) for batch in x_batch])
         else:
             x_data.append(x_batch)
-    return x_data if compress else [x_data]
+    if compress:
+        return preprocessing.scale(np.array(x_data))
+    else:
+        return [x_data]
+
+
+def vector_combine(file_list, y_list, data_dir, compress=False):
+    x_data = []
+    for i in range(0, len(file_list)):
+        x_batch = []
+        for s in ["TextVector", "WordVector", "ParagraphVec"]:
+            file_path = os.path.join(data_dir, "False" if y_list[i] == 1 else "Positive", s, file_list[i])
+            x_batch.extend(read_feature_file(file_path, s, get_config("seq_len")))
+        if compress:
+            x_data.append([sum(batch) / len(batch) for batch in x_batch])
+        else:
+            x_data.append(x_batch)
+    if compress:
+        return preprocessing.scale(np.array(x_data))
+    else:
+        return [x_data]
 
 
 def vector_graph(file_list, y_list, data_dir, feature_type):
@@ -76,7 +96,7 @@ def transform_predict_y(y_predict):
 
 
 def svm(X_train, X_test, y_train, y_test):
-    svc = SVC(kernel='rbf', verbose=True)
+    svc = SVC(kernel='rbf')
     svc.fit(X_train, y_train)
     evaluate(y_train, svc.predict(X_train), "SVM的训练集")
     evaluate(y_test, svc.predict(X_test), "SVM的测试集")
@@ -89,21 +109,44 @@ def gaussianNB(X_train, X_test, y_train, y_test):
     evaluate(y_test, clf.predict(X_test), "NB的测试集")
 
 
+def randomForest(X_train, X_test, y_train, y_test):
+    rfc = RandomForestClassifier()
+    rfc.fit(X_train,y_train)
+    evaluate(y_train, rfc.predict(X_train), "RF的训练集")
+    evaluate(y_test, rfc.predict(X_test), "RF的测试集")
+
+
 def nn(model_name, model, X_train, X_test, y_train, y_test):
-    model.fit(X_train, [transform_y(y_train)], epochs=10)
+    model.fit(X_train, [transform_y(y_train)], epochs=10, verbose=0)
     evaluate(y_train, transform_predict_y(model.predict(X_train)), model_name + "的训练集")
     evaluate(y_test, transform_predict_y(model.predict(X_test)), model_name + "的测试集")
 
 
 if __name__ == "__main__":
-    file_train, file_test, y_train, y_test = data_split(sys.argv[1])
-    # svm and nb
-    #X_train = vector_seq(file_train, y_train, sys.argv[1], compress=True)
-    #X_test = vector_seq(file_test, y_test, sys.argv[1], compress=True)
-    #svm(X_train, X_test, y_train, y_test)
-    #gaussianNB(X_train, X_test, y_train, y_test)
+
+
+    for s in ["TextVector", "WordVector", "ParagraphVec", "Combine"]:
+        print("#  " + s)
+        if s == "Combine":
+            s = "ParagraphVec"
+            file_train, file_test, y_train, y_test = data_split(sys.argv[1], s)
+            X_train = vector_combine(file_train, y_train, sys.argv[1], compress=True)
+            X_test = vector_combine(file_test, y_test, sys.argv[1], compress=True)
+        else:
+            file_train, file_test, y_train, y_test = data_split(sys.argv[1], s)
+            X_train = vector_seq(file_train, y_train, sys.argv[1], compress=True)
+            X_test = vector_seq(file_test, y_test, sys.argv[1], compress=True)
+        svm(X_train, X_test, y_train, y_test)
+        randomForest(X_train, X_test, y_train, y_test)
+        gaussianNB(X_train, X_test, y_train, y_test)
+
+        X_train = vector_seq(file_train, y_train, sys.argv[1])
+        X_test = vector_seq(file_test, y_test, sys.argv[1])
+        nn("cnn", cnn_model(16), X_train, X_test, y_train, y_test)
+        nn("lstm", lstm_model(16), X_train, X_test, y_train, y_test)
 
     # nn
+    #file_train, file_test, y_train, y_test = data_split(sys.argv[1], "ParagraphVec")
     #X_train = vector_seq(file_train, y_train, sys.argv[1])
     #X_test = vector_seq(file_test, y_test, sys.argv[1])
     #nn("cnn", cnn_model(16), X_train, X_test, y_train, y_test)
@@ -115,9 +158,9 @@ if __name__ == "__main__":
     #X_test, y_test = vector_graph(file_test, y_test, sys.argv[1], "DeepWalk")
     #nn("deepwalk_gcn", gcn_model(16), X_train, X_test, y_train, y_test)
     #
-    file_train, file_test, y_train, y_test = data_split(sys.argv[1], "ParagraphVec")
-    X_train, y_train = vector_graph(file_train, y_train, sys.argv[1], "ParagraphVec")
-    X_test, y_test = vector_graph(file_test, y_test, sys.argv[1], "ParagraphVec")
-    nn("para2vec_gcn", gcn_model(16), X_train, X_test, y_train, y_test)
+    #file_train, file_test, y_train, y_test = data_split(sys.argv[1], "ParagraphVec")
+    #X_train, y_train = vector_graph(file_train, y_train, sys.argv[1], "ParagraphVec")
+    #X_test, y_test = vector_graph(file_test, y_test, sys.argv[1], "ParagraphVec")
+    #nn("para2vec_gcn", gcn_model(16), X_train, X_test, y_train, y_test)
 
 
